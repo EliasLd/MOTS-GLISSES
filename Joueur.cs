122f1ad0b028d552b7a43e7504a2c137802c5182
Elias <elabd.elias@gmail.com>
2023-11-16 18:27:43 +0100

    Définition classe joueur, attributs et propriétés

---
+++ Joueur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOTS_GLISSES
+{
+    public class Joueur
+    {
+        private string nom;
+        private static string[] motTrouves = null;
+        private static int score = 0;
+
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+        }
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public string[] MotTrouves
+        {
+            get { return motTrouves;}
+        }
+    }
+}
