0484dfdcf0eb6e45aa9c9e2bbcd74d68826d622f
Elias <elabd.elias@gmail.com>
2023-11-16 22:28:18 +0100

    Ajout Quicksort + explication diviser pour régner

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -41,5 +41,54 @@ namespace MOTS_GLISSES
             sr.Close();
         }
 
+        /// <summary>
+        /// Fonction de tri rapide (Quicksort) Cette fonction utilise le principe de partitionnement et de divisier pour régner 
+        /// afin de trier notre dictionnaire (tableau de chaines de caractères). Concrètement, on choisit un élément du tableau que l'on va nommé pivot, 
+        /// d'après la documentation internet https://fr.wikipedia.org/wiki/Tri_rapide, il y a plusieurs choix de pivots possibles qui peuvent impacter le
+        /// temps d'éxecution de l'algo (ou du moins ses performances). Usuellement, on choisit le pivot comme étant le dernier élément du tableau.
+        /// Ensuite, on applique le partitionnement qui est décrit juste en dessous et qui nous retourne l'indice du pivot. A partir duquel on fait des appels 
+        /// récursifs des deux côtés du tableau (à gauche et droite du pivot). Enfin le programme s'arrête lorsque "début" atteint l'indice "fin" car cela
+        /// signifie que l'on a parcouru tout le tableau et qu'il est trié.
+        /// </summary>
+        /// <param name="tab">Dans notre cas, il s'agit du dicionnaire que l'on doit trier </param>
+        /// <param name="debut">littéralement le début du tableau, càd l'index de départ du tableau -> 0 (dans notre cas évidemment) </param>
+        /// <param name="fin">par analogie à "début", il s'agit du dernier indice du tableau dans notre cas car c'est tout le tableau que l'on souahite trier</param>
+        public void TriRapide(string[] tab, int debut, int fin)
+        {
+            if (debut < fin)   //Condition d'arrêt
+            {
+                int index_pivot = FairePartition(tab, debut, fin);
+                TriRapide(tab, debut, index_pivot - 1);
+                TriRapide(tab, index_pivot + 1, fin);
+            }
+        }
+
+        /// <summary>
+        /// Cette fontion fait en réalité partie du corps du tri rapide. Son vrai nom serait "Partitionnement"
+        /// et a pour but de réorganiser le tableau dans le but que tous les éléments plus petit que le pivot soit à un index inférieur au sien et que tous les éléments
+        /// plus grand que le pivot soient à un index supérieur au sien
+        /// </summary>
+        /// Les paramètres utilisés sont les mêmes que pour la fonction du tri rapide
+        /// <returns></returns>
+        public int FairePartition(string[] tab, int debut, int fin)
+        {
+            string pivot = tab[fin];   // -1 Pour éviter une erreur d'indice
+            int i = debut -1;
+            for(int j = debut; j <= fin - 1; j++) 
+            {
+                if (string.Compare(tab[j], pivot) <= 0)
+                {
+                    i++;
+                    string tmp = tab[i];       //On échange les éléments aux indices i et j
+                    tab[i] = tab[j];
+                    tab[j] = tmp;
+                }
+            }
+            string tmp2 = tab[i + 1];         //Puis on échange le pivot avec l'élément à l'indice i + 1
+            tab[i + 1] = tab[fin];
+            tab[fin] = tmp2;
+
+            return i + 1;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@
         {
             Dictionnaire dico = new Dictionnaire("francais");
             dico.RemplirDico("Mots_Français.txt");
-            Console.WriteLine(dico.GetDictionnaire.Length); // Test pour connaître la taille du dico, on fera des tests unitaires plus tard
+            Console.WriteLine(dico.GetDictionnaire.Length);                                // Test pour connaître la taille du dico, on fera des tests unitaires plus tard
+            dico.TriRapide(dico.GetDictionnaire, 0, dico.GetDictionnaire.Length - 1);     //On envoie l'indice - 1 pour une tentative d'accès à un indice inexistant
+            for(int i = 0; i < 500; i++)
+            {
+                Console.Write(dico.GetDictionnaire[i] + " ");                           //Test temporaire pour érifier que le tri fonctionne bien
+            }
         }
     }
 }
