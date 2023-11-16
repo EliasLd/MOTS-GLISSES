da61f67700e8c48c58f49ae67299dee3a1140a91
Elias <elabd.elias@gmail.com>
2023-11-16 21:24:14 +0100

    définition classe Dictionnaire StreamReader + sauvegarde du dictionnaire complet dans un tableau de string

---
+++ Dictionnaire.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MOTS_GLISSES
+{
+    public class Dictionnaire
+    {
+        private static string[] dictionnaire;
+        private static int[,] nombreMotParLettre = new int[26, 2];
+        private string langue;
+
+        public string[] GetDictionnaire
+        {
+            get { return dictionnaire; }
+        }
+
+        public Dictionnaire(string langue)
+        {
+            this.langue = langue;
+        }
+
+        public void RemplirDico(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    string lignes = sr.ReadToEnd();
+                    dictionnaire = lignes.Split(" ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur de lecture" + e.Message);
+            }
+            sr.Close();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-           
+            Dictionnaire dico = new Dictionnaire("francais");
+            dico.RemplirDico("Mots_Français.txt");
+            Console.WriteLine(dico.GetDictionnaire.Length); // Test pour connaître la taille du dico, on fera des tests unitaires plus tard
         }
     }
 }
