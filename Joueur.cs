7da6383a0d74a5c86c8a03d83d1379134d77d9e1
Elias <elabd.elias@gmail.com>
2023-11-16 19:36:39 +0100

    Ajout de toutes les méthodes : - Add_Mot -toString -Add_score -bool Contient

--- Joueur.cs
+++ Joueur.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace MOTS_GLISSES
     public class Joueur
     {
         private string nom;
-        private static string[] motTrouves = null;
+        private static string motTrouves = null;
         private static int score = 0;
 
         public Joueur(string nom)
@@ -24,12 +25,37 @@ namespace MOTS_GLISSES
         public int Score
         {
             get { return score; }
-            set { score = value; }
         }
 
-        public string[] MotTrouves
+        public string MotTrouves
         {
             get { return motTrouves;}
+        }
+
+        public void Add_Mot(string mot)
+        {
+            motTrouves += mot + " ";
+        }
+
+        public string toString()
+        {
+            return (this.nom + " a un score de " + score + " et a trouvé les mots suivant : " + motTrouves);
+        }
+
+        public void Add_Score(int val)
+        {
+            score += val;
+        }
+
+        public bool Contient(string mot)
+        {
+            string[] tab_mots = motTrouves.Split(' ');
+            for(int i = 0; i <  tab_mots.Length; i++) 
+            {
+                if (tab_mots[i] == mot)
+                    return true;
+            }
+            return false;
         }
     }
 }
